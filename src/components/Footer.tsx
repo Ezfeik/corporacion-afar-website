7032e44e843fc9ee9feb0ec163146c64c6842d57
Ezfeik <spvo_2007@hotmail.com>
2024-01-25 19:54:20 -0300

    Add initial footer styles

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="bg-blue-950 text-slate-200">
+      <div className="flex flex-row justify-center max-w-[1440px] h-auto mx-auto px-8 relative py-5">
+        © 2024 Corporación Afar. Todos los derechos reservados.
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
