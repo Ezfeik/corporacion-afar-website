ae1c2fc37e1285ee3075ea5b407ae597329bd693
Ezfeik <spvo_2007@hotmail.com>
2024-01-26 19:06:50 -0300

    Add new color to palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary': {
+          '50': '#ebf9ff',
+          '100': '#d1f1ff',
+          '200': '#aee8ff',
+          '300': '#76dcff',
+          '400': '#35c6ff',
+          '500': '#07a3ff',
+          '600': '#007dff',
+          '700': '#0064ff',
+          '800': '#0052d7',
+          '900': '#004295',
+          '950': '#062d65',
+        },
+      }
     },
   },
   plugins: [],
