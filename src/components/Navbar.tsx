b98f89782a7acab8da95db7f8d14d73cbaf4a19d
Ezfeik <spvo_2007@hotmail.com>
2024-01-26 19:11:10 -0300

    Update in names and styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,24 @@
 'use client'
 import Link from "next/link"
-import { linkType } from '@/types/navbarTypes';
+import { navLinkType } from '@/types/navbarTypes';
 import { ReactElement, useCallback, useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 
-const navListContainerClass = "bg-blue-100 lg:bg-transparent fixed lg:static top-0 -right-full h-[100vh] lg:h-fit w-[70%] lg:w-fit px-8 py-4 lg:p-0 transition-[right] duration-500";
+const navListContainerClass = "bg-primary-100 lg:bg-transparent fixed lg:static top-0 -right-full h-[100vh] lg:h-fit w-[70%] lg:w-fit px-8 py-4 lg:p-0 transition-[right] duration-500";
 const navListClass = "flex flex-col lg:flex-row items-start lg:items-center gap-6 lg:gap-8";
-const linkClass = "text-lg antialiased after:block after:w-0 after:opacity-0 after:border-b-2 after:border-b-slate-700 after:transition-all after:duration-200 hover:after:w-full hover:after:opacity-100";
-const linkClassActive = "text-lg antialiased after:block after:w-full after:border-b-2 after:border-b-slate-700";
+const linkClass = "text-lg text-gray-600 hover:text-gray-800 antialiased after:block after:w-0 after:opacity-0 after:border-b-2 after:border-b-gray-600 after:transition-all after:ease-in-out after:duration-300 hover:after:w-full hover:after:opacity-100";
+const linkClassActive = "text-lg text-gray-800 antialiased after:block after:w-full after:border-b-2 after:border-b-gray-800";
 const toggleButtonClass = "block lg:hidden"
 
-function NavbarList({ pathname, links, handleToggle }: { pathname: string, links: linkType[], handleToggle: Function }): ReactElement {
+function NavbarList({ pathname, links, handleToggle }: { pathname: string, links: navLinkType[], handleToggle: Function }): ReactElement {
   return (
     <div id="navbar-list" className={navListContainerClass}>
       <ul className={navListClass}>
-        <li className="block lg:hidden place-self-end">
+        <li className="block lg:hidden place-self-end mt-3">
           <ToggleButton show={false} handleToggle={handleToggle} />
         </li>
         {
-          links.map(( link: linkType, index: number ) => (
+          links.map(( link: navLinkType, index: number ) => (
             <li key={ index }>
               <Link className={ pathname === link.href ? linkClassActive : linkClass } href={ link.href }>
                 { link.name }
@@ -35,13 +35,16 @@ function ToggleButton({ show, handleToggle }: { show: boolean, handleToggle: Fun
   return (
     <button onClick={() => handleToggle()} className={toggleButtonClass}>
       { 
-        show ? (<svg width="40" height="38" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M18 7L6 7" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
-        <path d="M18 12L6 12" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
-        <path d="M18 17L6 17" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
-        </svg>) : (
-          <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M13.06 12L17.48 7.57996C17.5537 7.5113 17.6128 7.4285 17.6538 7.3365C17.6948 7.2445 17.7168 7.14518 17.7186 7.04448C17.7204 6.94378 17.7018 6.84375 17.6641 6.75036C17.6264 6.65697 17.5703 6.57214 17.499 6.50092C17.4278 6.4297 17.343 6.37356 17.2496 6.33584C17.1562 6.29811 17.0562 6.27959 16.9555 6.28137C16.8548 6.28314 16.7555 6.30519 16.6635 6.34618C16.5715 6.38717 16.4887 6.44627 16.42 6.51996L12 10.94L7.58 6.51996C7.43782 6.38748 7.24978 6.31535 7.05548 6.31878C6.86118 6.32221 6.67579 6.40092 6.53838 6.53834C6.40096 6.67575 6.32225 6.86113 6.31882 7.05544C6.3154 7.24974 6.38752 7.43778 6.52 7.57996L10.94 12L6.52 16.42C6.37955 16.5606 6.30066 16.7512 6.30066 16.95C6.30066 17.1487 6.37955 17.3393 6.52 17.48C6.66062 17.6204 6.85125 17.6993 7.05 17.6993C7.24875 17.6993 7.43937 17.6204 7.58 17.48L12 13.06L16.42 17.48C16.5606 17.6204 16.7512 17.6993 16.95 17.6993C17.1488 17.6993 17.3394 17.6204 17.48 17.48C17.6204 17.3393 17.6993 17.1487 17.6993 16.95C17.6993 16.7512 17.6204 16.5606 17.48 16.42L13.06 12Z" fill="#000000"/>
+        show ? (
+          <svg width="27" height="23" viewBox="0 0 27 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect y="10" width="27" height="3" rx="1.5" className="fill-gray-600"/>
+          <rect width="27" height="3" rx="1.5" className="fill-gray-600"/>
+          <rect y="20" width="27" height="3" rx="1.5" className="fill-gray-600"/>
+          </svg>
+        ) : (
+          <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect y="22.6274" width="32" height="3" rx="1.5" transform="rotate(-45 0 22.6274)" className="fill-gray-600"/>
+          <rect x="2.12134" width="32" height="3" rx="1.5" transform="rotate(45 2.12134 0)" className="fill-gray-600"/>
           </svg>
         ) 
       }
@@ -51,7 +54,7 @@ function ToggleButton({ show, handleToggle }: { show: boolean, handleToggle: Fun
   )
 }
 
-export default function Navbar({ links, logoSrc }: { links: linkType[], logoSrc: string }): ReactElement {
+export default function Navbar({ links, logo }: { links: navLinkType[], logo: { src: string, alt: string } }): ReactElement {
   const pathname = usePathname();
   const [isNavbarHidden, setIsNavbarHidden] = useState(true);
 
@@ -75,9 +78,9 @@ export default function Navbar({ links, logoSrc }: { links: linkType[], logoSrc:
   }, []);
 
   return (
-    <nav className="bg-blue-50 bg-opacity-95 shadow-sm sticky top-0 h-20 duration-500 ease-in-out transition-[top] z-50">
+    <nav className="bg-gray-100 bg-opacity-95 shadow-sm sticky top-0 h-20 duration-500 ease-in-out transition-[top] z-50">
       <div className="flex flex-row justify-between items-center max-w-[1440px] h-auto mx-auto px-8 relative">
-        <img className="h-20" src={logoSrc} alt="logo corporación afar" />
+        <img className="h-20" src={logo.src} alt={logo.alt} />
         <NavbarList 
           pathname={pathname} 
           links={links} 
