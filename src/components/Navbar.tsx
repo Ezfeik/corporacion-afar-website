8afea7cba739b1e34fc2427f0b868eb73bb0e9af
Ezfeik <spvo_2007@hotmail.com>
2024-01-30 17:22:44 -0300

    Add container wrapper to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,9 @@ import Link from "next/link"
 import { navLinkType } from '@/types/navbarTypes';
 import { ReactElement, useCallback, useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
+import Container from "./Container";
 
-const navListContainerClass = "bg-primary-100 lg:bg-transparent fixed lg:static top-0 -right-full h-[100vh] lg:h-fit w-[70%] lg:w-fit px-8 py-4 lg:p-0 transition-[right] duration-500";
+const navListContainerClass = "bg-gray-200 lg:bg-transparent fixed lg:static top-0 -right-full h-[100vh] lg:h-fit w-[70%] lg:w-fit px-8 py-4 lg:p-0 transition-[right] duration-500";
 const navListClass = "flex flex-col lg:flex-row items-start lg:items-center gap-6 lg:gap-8";
 const linkClass = "text-lg text-gray-600 hover:text-gray-800 antialiased after:block after:w-0 after:opacity-0 after:border-b-2 after:border-b-gray-600 after:transition-all after:ease-in-out after:duration-300 hover:after:w-full hover:after:opacity-100";
 const linkClassActive = "text-lg text-gray-800 antialiased after:block after:w-full after:border-b-2 after:border-b-gray-800";
@@ -36,13 +37,13 @@ function ToggleButton({ show, handleToggle }: { show: boolean, handleToggle: Fun
     <button onClick={() => handleToggle()} className={toggleButtonClass}>
       { 
         show ? (
-          <svg width="27" height="23" viewBox="0 0 27 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg width="24" height="24" viewBox="0 0 27 23" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect y="10" width="27" height="3" rx="1.5" className="fill-gray-600"/>
           <rect width="27" height="3" rx="1.5" className="fill-gray-600"/>
           <rect y="20" width="27" height="3" rx="1.5" className="fill-gray-600"/>
           </svg>
         ) : (
-          <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg width="22" height="22" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect y="22.6274" width="32" height="3" rx="1.5" transform="rotate(-45 0 22.6274)" className="fill-gray-600"/>
           <rect x="2.12134" width="32" height="3" rx="1.5" transform="rotate(45 2.12134 0)" className="fill-gray-600"/>
           </svg>
@@ -78,16 +79,18 @@ export default function Navbar({ links, logo }: { links: navLinkType[], logo: { 
   }, []);
 
   return (
-    <nav className="bg-gray-100 bg-opacity-95 shadow-sm sticky top-0 h-20 duration-500 ease-in-out transition-[top] z-50">
-      <div className="flex flex-row justify-between items-center max-w-[1440px] h-auto mx-auto px-8 relative">
-        <img className="h-20" src={logo.src} alt={logo.alt} />
-        <NavbarList 
-          pathname={pathname} 
-          links={links} 
-          handleToggle={handleToggle}
-        />
-        <ToggleButton show={true} handleToggle={handleToggle} />
+    <nav className="bg-opacity-95 shadow-sm sticky top-0 h-20 duration-500 ease-in-out transition-[top] z-50">
+      <Container bgColorClass="bg-gray-100">
+        <div className="flex flex-row justify-between items-center h-auto relative">
+          <img className="h-20" src={logo.src} alt={logo.alt} />
+          <NavbarList 
+            pathname={pathname} 
+            links={links} 
+            handleToggle={handleToggle}
+          />
+          <ToggleButton show={true} handleToggle={handleToggle} />
         </div>
+      </Container>
     </nav>
   )
 }
