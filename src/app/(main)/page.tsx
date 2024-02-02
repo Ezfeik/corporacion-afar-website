b4e0b7359fbafb51ef16e8533859eb2fef1aec79
Ezfeik <spvo_2007@hotmail.com>
2024-02-02 15:59:50 -0300

    Initial main view

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,29 @@
+import Container from "@/components/Container";
+
 export default function Page() {
   return (
-    <div className="">Inicio</div>
-  )
+    <>
+      <Container bgImgClass="bg-img-hands bg-center bg-cover grayscale brightness-[40%]">
+        <section className="py-28 relative">
+          <div className="w-[70%]">
+            <h1 className="text-5xl font-bold text-secondary-500 mb-14">En contra de la violencia</h1>
+            <p className="text-4xl text-white leading-relaxed">Creemos en el poder transformador de la comunicación, el don de saber escuchar, la toma de conciencia, la empatía y apoyo entre seres humanos.</p>
+          </div>
+        </section>
+      </Container>
+      <Container bgColorClass="bg-white">
+        <section className="py-8">
+          <h2 className="text-3xl text-primary-800">Noticias</h2>
+          <div className="grid grid-cols-3">
+            <article>Noticia 1</article>
+            <article>Noticia 2</article>
+            <article>Noticia 3</article>
+          </div>
+        </section>
+      </Container>
+      <Container bgColorClass="bg-white">
+        <h2 className="py-10 text-4xl font-bold text-center text-secondary-500">&quot;Nada es tan fuerte como la ayuda a través de la comprensión&quot;</h2>
+      </Container>
+    </>
+  );
 }
