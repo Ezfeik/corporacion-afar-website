4e580598a4d4d382d6bc0df5cd0381870b52c395
Ezfeik <spvo_2007@hotmail.com>
2024-02-05 15:29:35 -0300

    Update component to allow background images

--- src/components/sections/SectionTitle.tsx
+++ src/components/sections/SectionTitle.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-function SectionTitle({ colorClass = "text-secondary-500", children }: { colorClass?: string, children: string }) {
+function SectionTitle({ bgImgClass, colorClass = "text-secondary-500", children }: { bgImgClass?: string, colorClass?: string, children: string }) {
   const textStyleClass = "py-6 lg:py-10 text-4xl text-center font-bold antialiased"
-  const styleClass = colorClass ? textStyleClass + " " + colorClass : textStyleClass
+  let styleClass = textStyleClass + " " + colorClass;
+  styleClass = bgImgClass ? styleClass + " " + bgImgClass : styleClass
 
   return (
     <h1 className={styleClass}>{ children }</h1>
