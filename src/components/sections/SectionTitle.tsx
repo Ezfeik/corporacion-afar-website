8a9dc046a038c93ac3ae1e25973f55c524e92ab7
Ezfeik <spvo_2007@hotmail.com>
2024-02-08 19:34:36 -0300

    Update section title styles and adds decorators

--- src/components/sections/SectionTitle.tsx
+++ src/components/sections/SectionTitle.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
-function SectionTitle({ bgImgClass, colorClass = "text-secondary-500", children }: { bgImgClass?: string, colorClass?: string, children: string }) {
-  const textStyleClass = "py-6 lg:py-10 text-4xl text-center font-bold antialiased"
-  let styleClass = textStyleClass + " " + colorClass;
+function SectionTitle({ bgImgClass, textColorClass = "text-secondary-500", decoratorColorClass, children }: { bgImgClass?: string, textColorClass?: string, decoratorColorClass?: string, children: string }) {
+  const textStyleClass = "flex relative py-6 lg:py-10 text-3xl lg:text-4xl text-center font-bold antialiased w-fit mx-auto"
+  let styleClass = textStyleClass + " " + textColorClass;
   styleClass = bgImgClass ? styleClass + " " + bgImgClass : styleClass
 
+  const leftDecoratorStyleClass = decoratorColorClass ? decoratorColorClass + " w-12 h-1 my-auto me-4" : "";
+  const rightDecoratorStyleClass = decoratorColorClass ? decoratorColorClass + " w-12 h-1 my-auto ms-4" : "";
+
   return (
-    <h1 className={styleClass}>{ children }</h1>
+    <h1 className={styleClass}><span className={leftDecoratorStyleClass}></span>{ children }<span className={rightDecoratorStyleClass}></span></h1>
   )
 }
 
