d90985b0502050fd0c61435019c722d93ec1ca95
Ezfeik <spvo_2007@hotmail.com>
2024-02-08 19:32:27 -0300

    Add animation component handler using intersection observer

---
+++ src/components/animations/AnimationWrapper.tsx
@@ -0,0 +1,36 @@
+import React, { ReactElement, useEffect, useRef, useState } from 'react'
+
+function AnimationWrapper({ initialClass = "", animationClass, visited = false, threshold, children }: { initialClass?: string, animationClass: string, visited?: boolean, threshold?: number, children: ReactElement }) {
+  const [isVisible, setIsVisible] = useState(visited);
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const options: IntersectionObserverInit | undefined = threshold ? { threshold }: undefined;
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target)
+        }
+      })
+    }, options)
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    }
+
+  }, []);
+
+
+  return (
+    <div ref={elementRef} className={isVisible ? initialClass + ' ' + animationClass : initialClass}>{children}</div>
+  )
+}
+
+export default AnimationWrapper
