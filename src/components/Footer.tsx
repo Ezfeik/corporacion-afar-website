89555d5db3304e71d1979e038f97b3e339826376
Ezfeik <spvo_2007@hotmail.com>
2024-02-08 19:37:33 -0300

    Update styles for footer on icons and alignment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
     <footer>
       <Container bgColorClass='bg-primary-100'>
         <>
-          <div className='grid grid-cols-1 lg:grid-cols-3 max-w-[1440px] text-primary-950 py-8 border-b-[thin] border-b-primary-950'>
+          <div className='grid grid-cols-1 lg:grid-cols-3 items-center max-w-[1440px] text-primary-950 py-6 border-b-[thin] border-b-primary-300'>
             <div className='mb-4 lg:mb-0'>
               <img className='h-28 lg:h-32' src={logoAfar.src} alt={logoAfar.alt} />
               <p className='text-sm pe-32'>
@@ -38,13 +38,13 @@ export function Footer() {
               <h2 className='text-lg font-bold mb-2'>Contacto</h2>
               <ul className='flex flex-col gap-2 mb-4'>
                 <li className='flex flex-row gap-2'>
-                  <MailIcon colorClass={'stroke-primary-950'} width={24} height={24} />contacto@corporacionafar.cl
+                  <MailIcon colorClass={'stroke-primary-950'} width={22} height={24} />contacto@corporacionafar.cl
                 </li>
                 <li className='flex flex-row gap-2'>
-                  <PhoneIcon colorClass={'stroke-primary-950'} width={24} height={24} />+56-91234578
+                  <PhoneIcon colorClass={'stroke-primary-950'} width={22} height={22} />+56-91234578
                 </li>
                 <li className='flex flex-row gap-2'>
-                  <PhoneIcon colorClass={'stroke-primary-950'} width={24} height={24} />+56-91234578
+                  <PhoneIcon colorClass={'stroke-primary-950'} width={22} height={22} />+56-91234578
                 </li>
               </ul>
               <SocialNetworks fillColorClass="fill-primary-950" />
