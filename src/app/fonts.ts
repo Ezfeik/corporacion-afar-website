212b51c444ad49fc8fe8cf459115517c9660d0d9
Ezfeik <spvo_2007@hotmail.com>
2024-02-09 17:54:14 -0300

    Rename font for consistency

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter, Libre_Baskerville } from 'next/font/google';
 
 export const inter = Inter({ weight: 'variable', subsets: ['latin'] });
-export const satisfy = Libre_Baskerville({ weight: '400', subsets: ['latin'] });
+export const libreBaskerville = Libre_Baskerville({ weight: '400', subsets: ['latin'] });
--- src/components/sections/EndPhrase.tsx
+++ src/components/sections/EndPhrase.tsx
@@ -1,9 +1,9 @@
-import { satisfy } from '@/app/fonts'
+import { libreBaskerville } from '@/app/fonts'
 import React from 'react'
 
 function EndPhrase({ textColorClass, children }: { textColorClass: string, children: string }) {
   return (
-    <h2 className={`${satisfy.className} py-10 text-3xl lg:text-4xl text-center ${textColorClass}`}>&quot;{ children }&quot;</h2>
+    <h2 className={`${libreBaskerville.className} py-10 text-3xl lg:text-4xl text-center ${textColorClass}`}>&quot;{ children }&quot;</h2>
   )
 }
 
