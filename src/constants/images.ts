b7c77131db925b6e650cf9dbbb603dbf23212c77
Ezfeik <spvo_2007@hotmail.com>
2024-02-09 17:57:32 -0300

    Add new images

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -2,3 +2,11 @@ export const logoAfar = {
   src: '/img/logo-sin-bg-difu-480px.webp',
   alt: 'logo Corporación Afar'
 } 
+export const handsImg = {
+  src: '/img/backgrounds/hands-loop.jpg',
+  alt: 'Imagen de manos unidas formando una estrella'
+} 
+export const talkImg = {
+  src: '/img/backgrounds/talk.jpg',
+  alt: 'Dos personas desde la cintura para abajo tomandose las manos sentados en una cama'
+} 
