bba531334baf3f08b7ae1ae39333d762bf379b31
Ezfeik <spvo_2007@hotmail.com>
2024-02-09 17:52:07 -0300

    Move EndPhrase component and add custom font

--- src/components/EndPhrase.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function EndPhrase({ textColorClass, children }: { textColorClass: string, children: string }) {
-  return (
-    <h2 className={"py-10 text-3xl lg:text-4xl font-bold text-center " + textColorClass}>&quot;{ children }&quot;</h2>
-  )
-}
-
-export default EndPhrase
---
+++ src/components/sections/EndPhrase.tsx
@@ -0,0 +1,10 @@
+import { satisfy } from '@/app/fonts'
+import React from 'react'
+
+function EndPhrase({ textColorClass, children }: { textColorClass: string, children: string }) {
+  return (
+    <h2 className={`${satisfy.className} py-10 text-3xl lg:text-4xl text-center ${textColorClass}`}>&quot;{ children }&quot;</h2>
+  )
+}
+
+export default EndPhrase
