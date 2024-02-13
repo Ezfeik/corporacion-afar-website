e4b7553d0437fb2f382ca7a82a5d4cd3793b578d
Ezfeik <spvo_2007@hotmail.com>
2024-02-13 13:39:07 -0300

    Move components to respective section folder

---
+++ src/components/sections/biblioteca/Book.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { bookType } from '@/types/bookTypes'
+
+function Book({ data }: { data: bookType }) {
+  return (
+    <a target="_blank" href={data.href} style={{backgroundImage: `url('${data.coverSrc !== "" ? data.coverSrc : "/img/book-covers/template.jpg"}')`}} className={"relative bg-center bg-cover border-8 border-tertiary-100 bg-tertiary-50 mx-auto w-40 h-60 md:w-52 md:h-72 scale-100 hover:scale-[1.02] transition-[transform] duration-200 ease-in"}>
+    </a>
+  )
+}
+
+export default Book
