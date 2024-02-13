3e49d17aa6c733dda2c7027da7877dbd6cb16f9f
Ezfeik <spvo_2007@hotmail.com>
2024-02-13 13:41:19 -0300

    Add responsiveness

--- src/app/(main)/nosotros/page.tsx
+++ src/app/(main)/nosotros/page.tsx
@@ -23,7 +23,7 @@ const members: MemberType[] = [
 
 const TeamCardList = function({ members }: { members: MemberType[] }) {
   return (
-    <ul className='grid grid-cols-3'>
+    <ul className='grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-0'>
       { members.map((member) => <TeamCard key={member.name} member={member} />) }
     </ul>
   )
@@ -44,15 +44,15 @@ const TeamCard = function({ member }: { member: MemberType }) {
 
 const AboutUsContainer = function() {
   return (
-    <div className='grid grid-cols-3 py-12 mx-auto'>
-      <AboutUsContent title="Misión" icon={<FlagIcon width={36} height={36} colorClass='stroke-secondary-700' />}>
+    <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 lg:gap-0 py-10 lg:py-12 mx-auto'>
+      <AboutUsContent title="Misión" icon={<FlagIcon width='w-8 lg:w-9' height='w-8 lg:h-9' colorClass='stroke-secondary-700' />}>
         Queremos construir una sociedad igualitaria y libre de violencia, a través de la empatía 
         y una atención global para las víctimas.
       </AboutUsContent>
-      <AboutUsContent title="Visión" icon={<EyeIcon width={36} height={36} colorClass='stroke-secondary-700' />}>
+      <AboutUsContent title="Visión" icon={<EyeIcon width='w-8 lg:w-9' height='w-8 lg:h-9' colorClass='stroke-secondary-700' />}>
         Liberar el potencial de las victimas supervivientes como agentes de cambio social.
       </AboutUsContent>
-      <AboutUsContent title="Nuestros Valores" icon={<HeartIcon width={36} height={36} colorClass='stroke-secondary-700' />}>
+      <AboutUsContent title="Nuestros Valores" icon={<HeartIcon width='w-8 lg:w-9' height='w-8 lg:h-9' colorClass='stroke-secondary-700' />}>
         <>
           Como Corporación Afar, la <b>empatía</b>, el <b>respeto</b> y la <b>autonomia</b> son lo que nos caracteriza y nos lleva a dar
           lo mejor de nosotros.
@@ -64,9 +64,9 @@ const AboutUsContainer = function() {
 
 const AboutUsContent = function({ title, icon, children }: { title: string, icon: ReactElement, children: string | ReactElement }) {
   return (
-    <div className='px-12'>
-      <h2 className='flex justify-center gap-3 text-secondary-700 font-bold text-3xl mb-6'><span className='inline w-auto'>{ icon }</span>{ title }</h2>
-      <p className='text-secondary-700 text-xl text-center leading-relaxed'>{ children }</p>
+    <div className='px-4 lg:px-12'>
+      <h2 className='flex justify-center gap-3 text-secondary-700 font-bold text-2xl lg:text-3xl mb-3 lg:mb-6'><span className='inline w-auto'>{ icon }</span>{ title }</h2>
+      <p className='text-secondary-700 text-lg lg:text-xl text-center leading-relaxed'>{ children }</p>
     </div>
   )
 }
@@ -75,40 +75,42 @@ function Page() {
   return (
     <section>
       <Container bgImgClass='bg-img-logo bg-center bg-cover opacity-[3%]'>
-        <div className='py-24'>
-          <SectionTitle textColorClass="text-secondary-500" decoratorColorClass="bg-secondary-500">Sobre Nosotros</SectionTitle>
-          <p className='w-3/5 mx-auto mt-12 text-2xl leading-relaxed text-center text-gray-800'>
+        <>
+          <div className='pt-12 lg:pt-24 pb-6 lg:pb-12'>
+            <SectionTitle textColorClass="text-secondary-500" decoratorColorClass="bg-secondary-500">Sobre Nosotros</SectionTitle>
+          </div>
+          <p className='w-[90%] lg:w-3/5 mx-auto pb-12 lg:pb-24 text-xl lg:text-2xl leading-relaxed text-center text-gray-800'>
             Somos una corporación privada, sin fines de lucro que protege y defiende los derechos de las víctimas de violencia intrafamiliar hacia
             mujeres, niños, adolescentes, hombres, padres.
           </p>
-        </div>
+        </>
       </Container>
       <Container bgColorClass='bg-secondary-50'>
         <AboutUsContainer />
       </Container>
       <Container>
-        <div className='py-24'>
+        <div className='py-12 lg:py-24'>
           <div>
             <ImgText 
               img={talkImg} 
               title='Apoyamos'
-              text={'Damos atención a víctimas de violencia de cualquier fuente mediante un modelo de intervención integral, con énfasis en la prevención y acompañado de una reparación y restauración del grupo familiar.'}  
+              text={'Entregamos atención a víctimas de violencia de cualquier fuente mediante un modelo de intervención integral, con énfasis en la prevención y acompañado de una reparación y restauración del grupo familiar.'}  
             />
           </div>
-          <div className='mt-16'>
+          <div className='mt-8 lg:mt-16'>
             <ImgText 
               img={handsImg} 
               title='Concientizamos' 
-              text={'Promovemos proyectos a importantes entidades, realizamos capacitaciones del ambito psicosocial / jurídico y talleres preventivos de violencia dirigidas a todo público.'} 
+              text={'Promovemos proyectos a importantes entidades, realizamos capacitaciones del ambito psicosocial/jurídico y talleres preventivos de violencia dirigidas a todo público.'} 
               reverse
             />
           </div>
         </div>
       </Container>
       <Container>
-        <div className='pb-24'>
+        <div className='pb-12 lg:pb-24'>
           <SectionTitle textColorClass="text-secondary-500" decoratorColorClass="bg-secondary-500">Nuestro Equipo</SectionTitle>
-          <div className='pt-14'>
+          <div className='pt-7 lg:pt-14'>
             <TeamCardList members={members} />
           </div>
         </div>
