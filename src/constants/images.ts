73965bc7b7ada801d231114b2216b2503a9b63e3
Ezfeik <spvo_2007@hotmail.com>
2024-02-14 18:24:15 -0300

    Add posters imgs

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,3 +1,5 @@
+import { imageType } from "@/types/imgTypes"
+
 export const logoAfar = {
   src: '/img/logo-sin-bg-difu-480px.webp',
   alt: 'logo Corporación Afar'
@@ -10,3 +12,15 @@ export const talkImg = {
   src: '/img/backgrounds/talk.jpg',
   alt: 'Dos personas desde la cintura para abajo tomandose las manos sentados en una cama'
 } 
+export const posters: { [key: string]: imageType } = { 
+    angela: { src: '/img/posters/angela.png', alt: '' },
+    charlotte: { src: '/img/posters/charlotte.png', alt: '' },
+    hombre1: { src: 'img/posters/hombre-1.png', alt: '' },
+    hombre2: { src: 'img/posters/hombre-2.png', alt: '' },
+    indicadores: { src: 'img/posters/indicadores.jpg', alt: '' },
+    jiddu: { src: 'img/posters/jiddu.png', alt: '' },
+    mujer1: { src: 'img/posters/mujer1.png', alt: '' },
+    poderViolencia: { src: 'img/posters/poder-violencia.png', alt: '' },
+    vejez: { src: 'img/posters/vejez.jpg', alt: '' },
+    virginia: { src: 'img/posters/virginia.png', alt: '' },
+  }
