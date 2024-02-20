b89bbb19bc8c748f9d013211bf9fedb2dfc45c41
Ezfeik <spvo_2007@hotmail.com>
2024-02-20 18:10:51 -0300

    Move NewsCardList to its own file

---
+++ src/components/sections/noticias/NewsCardList.tsx
@@ -0,0 +1,21 @@
+import newsData from '@/data/news.json';
+import { NewsCard } from './NewsCard';
+
+export const NewsCardList = function() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-6">
+      { 
+        newsData.map((news) => (
+          <NewsCard 
+            key={news.id}
+            id={news.id}
+            date={new Date(news.date)} 
+            title={news.title} 
+            cover={news.cover} 
+            text={news.text} 
+          />
+        ))
+      }
+    </div>
+  )
+}
