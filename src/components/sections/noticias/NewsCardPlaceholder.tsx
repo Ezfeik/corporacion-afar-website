206f023d0fbd0fad1567219d863b58b9e7315bb8
Ezfeik <spvo_2007@hotmail.com>
2024-02-20 18:09:57 -0300

    Create placeholder for news that still not load

---
+++ src/components/sections/noticias/NewsCardPlaceholder.tsx
@@ -0,0 +1,10 @@
+export const NewsCardPlaceholder = function() {
+  return (
+    <div className="opacity-50 border-2 border-gray-100 shadow-lg rounded-sm">
+      <div className="h-56 bg-gray-300"></div>
+      <div className="px-6 pb-6">
+        <div className="h-64"></div>
+      </div>
+    </div>
+  )
+}
