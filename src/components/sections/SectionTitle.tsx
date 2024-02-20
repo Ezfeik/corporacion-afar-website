81d2635b4832c4898f07b294cfc5bc404cf31288
Ezfeik <spvo_2007@hotmail.com>
2024-02-20 18:05:10 -0300

    Add spacing to decorators

--- src/components/sections/SectionTitle.tsx
+++ src/components/sections/SectionTitle.tsx
@@ -5,8 +5,8 @@ function SectionTitle({ bgImgClass, textColorClass = "text-secondary-500", decor
   let styleClass = textStyleClass + " " + textColorClass;
   styleClass = bgImgClass ? styleClass + " " + bgImgClass : styleClass
 
-  const leftDecoratorStyleClass = decoratorColorClass ? decoratorColorClass + " w-12 h-1 my-auto me-4" : "";
-  const rightDecoratorStyleClass = decoratorColorClass ? decoratorColorClass + " w-12 h-1 my-auto ms-4" : "";
+  const leftDecoratorStyleClass = decoratorColorClass ? decoratorColorClass + " w-12 h-1 my-auto me-6" : "";
+  const rightDecoratorStyleClass = decoratorColorClass ? decoratorColorClass + " w-12 h-1 my-auto ms-6" : "";
 
   return (
     <h1 className={styleClass}><span className={leftDecoratorStyleClass}></span>{ children }<span className={rightDecoratorStyleClass}></span></h1>
