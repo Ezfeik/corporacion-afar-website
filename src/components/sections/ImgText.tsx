74b39d3d4edd2b69a1fe83b204566bfff2ad4caf
Ezfeik <spvo_2007@hotmail.com>
2024-03-03 17:30:32 -0300

    Add animation for img and size change

--- src/components/sections/ImgText.tsx
+++ src/components/sections/ImgText.tsx
@@ -14,11 +14,15 @@ type ImgTextType = {
 function ImgText({ img, title, titleColorClass = 'text-gray-800', text, textColorClass = 'text-gray-700', reverse }: ImgTextType) {
   return (
     <div className='grid grid-cols-1 lg:grid-cols-2'>
-      <img className={`mx-auto brightness-75 lg:w-4/5 ${reverse ? 'order-1 lg:order-2' : 'order-1'}`} src={img.src} alt={img.alt} />
+      <div className={`mx-auto brightness-75 lg:w-3/4 ${reverse ? 'order-1 lg:order-2' : 'order-1'}`}>
+        <AnimationWrapper initialClass='lg:opacity-0' animationClass="lg:animate-[800ms_opacity-in-75_ease-in-out_forwards]" threshold={0.5}>
+          <img src={img.src} alt={img.alt} />
+        </AnimationWrapper>
+      </div>
         <div className={`w-full lg:w-2/3 mx-auto ${reverse ? 'order-2 lg:order-1' : 'order-2'}`}>
-          <AnimationWrapper initialClass='lg:opacity-0' animationClass={reverse ? 'lg:animate-[800ms_fade-in-left_ease-in-out_forwards]' : 'lg:animate-[800ms_fade-in-right_ease-in-out_forwards]'} threshold={0.4}>
+          <AnimationWrapper initialClass='lg:opacity-0' animationClass={reverse ? 'lg:animate-[800ms_fade-in-left_ease-in-out_forwards]' : 'lg:animate-[800ms_fade-in-right_ease-in-out_forwards]'} threshold={0.5}>
             <>
-              <h3 className={`${titleColorClass} text-3xl lg:text-4xl font-bold text-center py-8 lg:py-12`}>{title}</h3>
+              <h3 className={`${titleColorClass} text-3xl lg:text-4xl font-bold text-center py-4 lg:py-8`}>{title}</h3>
               <p className={`${textColorClass} text-xl lg:text-2xl leading-relaxed`}>{text}</p>
             </>
           </AnimationWrapper>
