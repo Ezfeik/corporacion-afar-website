cc622841866da69aedb10f4765a8a41f0b5d7f15
Ezfeik <spvo_2007@hotmail.com>
2024-03-03 17:32:50 -0300

    Fix date to string function

--- src/components/sections/noticias/NewsCard.tsx
+++ src/components/sections/noticias/NewsCard.tsx
@@ -8,7 +8,7 @@ export const NewsCard = function( { id, date, title, cover, text }: NewsType) {
       <div className="px-6 pb-6">
         <h2><Link className="text-xl font-bold underline text-primary-700 hover:text-primary-500" href={`/noticias/${id}`}>{title}</Link></h2>
         {/* SOLUCION RANDOM */}
-        <p className="text-gray-500 text-sm mb-4">{new Date(date || '').toLocaleString()}</p>
+        <p className="text-gray-500 text-sm mb-4">{new Date(date || '').toLocaleDateString()}</p>
         {/* SOLUCION RANDOM */}
         <p className="max-h-40 line-clamp-5">{text}</p>
       </div>
