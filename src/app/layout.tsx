ea4a16891aff15c44603d83c40e720125bed2a90
Ezfeik <spvo_2007@hotmail.com>
2024-03-03 17:27:32 -0300

    Remove metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,5 @@
-import type { Metadata } from 'next'
 import { inter } from './fonts'
 import './globals.css'
-
-export const metadata: Metadata = {
-  title: 'Corporación Afar',
-  description: 'Landing page de corporación en ayuda a las familias en entorno de violencia intrafamiliar',
-  robots: 'noindex, nofollow',
-}
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
 
