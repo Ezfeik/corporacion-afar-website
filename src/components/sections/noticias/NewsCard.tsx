306fbc0a024b9bbaa6bd4951e76b849a20b0a5a2
Ezfeik <spvo_2007@hotmail.com>
2024-03-17 19:52:24 -0300

    Change date to momentjs parser

--- src/components/sections/noticias/NewsCard.tsx
+++ src/components/sections/noticias/NewsCard.tsx
@@ -1,14 +1,15 @@
-import { NewsType } from "@/types/newsTypes"
-import Link from "next/link"
+import { NewsType } from "@/types/newsTypes";
+import Link from "next/link";
+import moment from "moment";
 
 export const NewsCard = function( { id, date, title, cover, text }: NewsType) {
   return (
     <div className="border-2 border-gray-100 shadow-lg rounded-sm">
-      <img className="min-h-56 max-h-56 w-full object-cover mb-4" src={cover!.src} alt={cover!.alt}></img>
+      <Link href={`/noticias/${id}`}><img className="min-h-56 max-h-56 w-full object-cover mb-4" src={cover!.src} alt={cover!.alt}></img></Link>
       <div className="px-6 pb-6">
         <h2><Link className="text-xl font-bold underline text-primary-700 hover:text-primary-500" href={`/noticias/${id}`}>{title}</Link></h2>
         {/* SOLUCION RANDOM */}
-        <p className="text-gray-500 text-sm mb-4">{new Date(date || '').toLocaleDateString()}</p>
+        <p className="text-gray-500 text-sm mb-4">{moment(date).format("DD/MM/YYYY")}</p>
         {/* SOLUCION RANDOM */}
         <p className="max-h-40 line-clamp-5">{text}</p>
       </div>
