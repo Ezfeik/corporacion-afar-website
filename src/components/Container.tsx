999a2506e3110458d9dc51c41dfe13272fe55921
Ezfeik <spvo_2007@hotmail.com>
2024-03-17 19:52:03 -0300

    Fix to background in library section being overlapping with some elements

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from 'react'
 function Container({ bgColorClass, bgImgClass, children }: { bgColorClass?: string, bgImgClass?: string, children: ReactElement }) {
   return (
     <div className={bgColorClass ?? "relative"}>
-      { bgImgClass && <div className={bgImgClass + " absolute top-0 left-0 w-full h-full"}></div> }
+      { bgImgClass && <div className={bgImgClass + " absolute -z-50 top-0 left-0 w-full h-full"}></div> }
       <div className='max-w-[1440px] mx-auto px-4 lg:px-8'>
         {children}
       </div>
