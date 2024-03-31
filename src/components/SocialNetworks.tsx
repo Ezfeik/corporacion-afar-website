403a2451c5581c448cb7bd850818518119aa05d6
Ezfeik <spvo_2007@hotmail.com>
2024-03-31 20:29:10 -0300

    Add linkedin to social networks

--- src/components/SocialNetworks.tsx
+++ src/components/SocialNetworks.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import InstagramIcon from './icons/Instagram'
 import FacebookIcon from './icons/Facebook'
+import LinkedinIcon from './icons/Linkedin'
 
 function SocialNetworks({ fillColorClass, width='w-7', height='h-7' } : { fillColorClass: string, width?: string, height?: string }) {
   return (
@@ -11,6 +12,9 @@ function SocialNetworks({ fillColorClass, width='w-7', height='h-7' } : { fillCo
       </Link>
       <Link href='https://www.facebook.com/corporacionafar/' target='_blank'>
         <FacebookIcon colorClass={fillColorClass} width={width} height={height} />
+      </Link>
+      <Link href='https://www.linkedin.com/in/corporaci%C3%B3n-afar-ayuda-a-la-familia-en-riesgo-42990a301/' target='_blank'>
+        <LinkedinIcon colorClass={fillColorClass} width={width} height={height} />
       </Link>
     </div>
   )
